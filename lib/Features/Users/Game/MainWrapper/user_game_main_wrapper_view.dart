2aece4580f17a9cff98980a203d2922a6ed83336
erfan <ehoseyni1383@gmail.com>
2024-08-01 00:57:34 +0330

    update

--- lib/Features/Users/Game/MainWrapper/user_game_main_wrapper_view.dart
+++ lib/Features/Users/Game/MainWrapper/user_game_main_wrapper_view.dart
@@ -1,49 +1,43 @@
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:gs/Features/Users/Game/MainWrapper/Mining/user_game_main_wrapper_mining_view.dart';
 import 'package:gs/Features/Users/Game/MainWrapper/user_game_main_wrapper_viewmodel.dart';
-import 'package:gs/Features/Users/Registration/Gender/user_registration_gender_view.dart';
-import 'package:gs/Features/Users/Registration/UserName/user_registration_username_view.dart';
+
+import '../../Registration/Gender/user_registration_gender_view.dart';
+import '../../Registration/UserName/user_registration_username_view.dart';
+import 'Mining/user_game_main_wrapper_mining_view.dart';
 
 class UserGameMainWrapperView extends StatelessWidget {
   UserGameMainWrapperView({super.key});
 
-  final UserGameMainWrapperViewModel userGameMainWrapperViewModel =
-  Get.put(UserGameMainWrapperViewModel());
+  final UserGameMainWrapperViewModel userGameMainWrapperViewModel = Get.put(UserGameMainWrapperViewModel());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(67, 155, 171, 1.0),
       body: GetBuilder<UserGameMainWrapperViewModel>(
-              builder: (context) {
-      return PageView(
-        onPageChanged: (value){
-          userGameMainWrapperViewModel.changeIndex(value);
-        },
-        controller: userGameMainWrapperViewModel.pageController,
-        children: [
+          builder: (context) => PageView(
+          onPageChanged: userGameMainWrapperViewModel.onPageChanged,
+          controller: userGameMainWrapperViewModel.pageController,
+          children: [
+            UserRegistrationUsernameView(),
+            UserGameMainWrapperMiningView(),
+            UserRegistrationGenderView(),
 
-          UserRegistrationUsernameView(),
-          UserGameMainWrapperMiningView(),
-          UserRegistrationGenderView(),
-
-        ],);
-        }
+          ],
+        ),
       ),
-      bottomNavigationBar: CustonNavigatorBar(),
+      bottomNavigationBar: customNavigatorBar(),
     );
   }
 
-  Obx CustonNavigatorBar() => Obx(
+  Obx customNavigatorBar() => Obx(
         () => ConvexAppBar(
       style: TabStyle.react,
-
       backgroundColor: const Color.fromRGBO(41, 41, 41, 1.0),
-      activeColor: Color.fromRGBO(67, 155, 171, 1.0),
+      activeColor: const Color.fromRGBO(67, 155, 171, 1.0),
       shadowColor: Colors.blueGrey,
-
       items: const [
         TabItem(
           icon: Icons.shopping_cart,
@@ -52,9 +46,10 @@ class UserGameMainWrapperView extends StatelessWidget {
         TabItem(icon: Icons.monetization_on_outlined, title: 'Mining',),
         TabItem(icon: Icons.business, title: 'Empire'),
       ],
+      controller: userGameMainWrapperViewModel.tabController,
       initialActiveIndex: userGameMainWrapperViewModel.selectedIndex.value,
-      onTap: (value) {
-        userGameMainWrapperViewModel.changeIndex(value);
+      onTap: (int value) {
+        userGameMainWrapperViewModel.changeTabIndex(value);
       },
     ),
   );
--- lib/Features/Users/Game/MainWrapper/user_game_main_wrapper_viewmodel.dart
+++ lib/Features/Users/Game/MainWrapper/user_game_main_wrapper_viewmodel.dart
@@ -1,17 +1,39 @@
 
 
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class UserGameMainWrapperViewModel extends GetxController{
+class UserGameMainWrapperViewModel extends GetxController with GetTickerProviderStateMixin {
+  late TabController tabController;
   var selectedIndex = 1.obs;
-  var pageController = PageController(initialPage: 1);
+  PageController pageController = PageController(initialPage: 1);
 
-  void changeIndex(int index) {
-    selectedIndex.value = index;
-    pageController.jumpToPage(index);
-    update();
+  @override
+  void onInit() {
+    tabController = TabController(
+      length: 3,
+      vsync: this,
+      initialIndex: 1,
+      animationDuration: const Duration(milliseconds: 300),
+    );
+    super.onInit();
   }
 
+  @override
+  void onClose() {
+    tabController.dispose();
+    super.onClose();
+  }
 
+  void changeTabIndex(int index) {
+    tabController.animateTo(index);
+    pageController.animateToPage(index, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
+    selectedIndex.value = index;
+  }
+
+  void onPageChanged(int index) {
+    selectedIndex.value = index;
+    tabController.animateTo(index);
+  }
 }
